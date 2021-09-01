ff760e2aa689ee28bad1dcc9ce06db09acf8718c
ashish-greycube <mr.ashish.shah@gmail.com>
2021-09-01 18:56:34 +0530

    start

--- amcg/amcg/doctype/booking_ct/booking_ct.js
+++ amcg/amcg/doctype/booking_ct/booking_ct.js
@@ -2,7 +2,52 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Booking CT', {
-	// refresh: function(frm) {
+	setup: function (frm) {
+		filter_item_based_on_item_group(frm)
+		set_price_list(frm)
+	},
+	item_group: function (frm) {
+		filter_item_based_on_item_group(frm)
+	},
+	customer: function (frm) {
+		set_price_list(frm)
+	}
 
-	// }
 });
+
+function filter_item_based_on_item_group(frm) {
+	if (frm.doc.item_group) {
+		frm.set_query('item_code', () => {
+			return {
+				query: 'amcg.amcg.doctype.booking_ct.booking_ct.get_items_in_item_group',
+				filters: {
+					item_group: frm.doc.item_group
+				}
+			}
+		})
+	}
+}
+
+function set_price_list(frm) {
+if (frm.doc.customer) {
+	let default_price_list
+	frappe.db.get_value('Customer', frm.doc.customer, 'default_price_list')
+			.then(r => {
+					default_price_list=r.message.default_price_list
+					if (!default_price_list) {
+						frappe.db.get_single_value('Selling Settings', 'selling_price_list')
+						.then(selling_price_list => {
+							default_price_list=selling_price_list
+							if (default_price_list) {
+								frm.set_value('price_list', default_price_list)							
+							}
+						})
+									
+					}else{
+						frm.set_value('price_list', default_price_list)	
+					}
+			})	
+}	
+
+
+}
--- amcg/amcg/doctype/booking_ct/booking_ct.py
+++ amcg/amcg/doctype/booking_ct/booking_ct.py
@@ -3,8 +3,27 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class BookingCT(Document):
 	pass
+
+
+
+# get all items that are descendants of a item group 
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def get_items_in_item_group(doctype, txt, searchfield, start, page_len, filters):
+	child_groups = ", ".join(['"' + i[0] + '"' for i in get_child_groups(filters.get('item_group'))])
+	result= frappe.db.sql("""select  I.item_name from `tabItem` I
+		where I.docstatus = 0 
+		and I.disabled = 0
+		and (I.item_group in (%s))""" % (child_groups))
+	return result
+
+def get_child_groups(item_group_name):
+	item_group = frappe.get_doc("Item Group", item_group_name)
+	return frappe.db.sql("""select name
+		from `tabItem Group` where lft>=%(lft)s and rgt<=%(rgt)s
+			""", {"lft": item_group.lft, "rgt": item_group.rgt})
